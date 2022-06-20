3f8830f8465ef0a65827b2c93c7a33f1a79097ba
raman186 <goyalraman755@gmail.com>
2022-06-20 21:56:10 +0530

    addd

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,5 +3,11 @@ import React, { createContext } from "react";
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
+  const [cartCount, setCartCount] = React.useState(0);
+
+  const handleCartCount = (val) => {
+    //+2
+    setCartCount(cartCount + val);
+  };
   return <CartContext.Provider>{children}</CartContext.Provider>;
 };
